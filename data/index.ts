c07b74bce2a5ed8e61c13113645d0bedb1637ec5
Md Fahim Faysal <faysalfahim795@gmail.com>
2026-06-02 17:43:02 +0600

    Resume uploaded

--- data/index.ts
+++ data/index.ts
@@ -9,7 +9,7 @@ export const siteConfig = {
   github: "https://github.com/faysalfahim",
   linkedin: "https://www.linkedin.com/in/fahim-faysal-a9750019b/",
   researchGate: "https://www.researchgate.net/profile/Md-Fahim-Faysal-2",
-  resumeUrl: "/resume.pdf",
+  resumeUrl: "/Md_Fahim_Faysal_Resume.pdf",
 };
 
 export type PublicationStatus =
